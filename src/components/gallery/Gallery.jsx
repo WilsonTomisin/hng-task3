1b54f672b3d2ca128c5a08271c723dcbe49428f3
Wilson Tomisin <tommywilson972@gmail.com>
2023-09-20 09:27:01 +0100

    implemented drag and drop feature

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import './Gallery.css'
 import {collection, getDoc} from 'firebase/firestore';
 import { db } from '../../firebase';
+import { Button, Box, Typography, Grid} from '@mui/material';
+import { imgGallery } from '../../constants/constants';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 export const Gallery = () => {
     const [user, setuser] = React.useState([])
+    const [gallery, setGallery] = React.useState(imgGallery)
 
     const usersCollectionRef = collection(db,'users')
 
@@ -14,9 +18,77 @@ export const Gallery = () => {
         }
         getUser()
     },[])
+    const handleDrag=(results)=>{
+      const {source, destination} = results
+      if (!destination) return ;
+      if (source.index == destination.index)  return;
+
+      const ReorderedItems = [...gallery]
+      const [reorderedItem] = ReorderedItems.splice(source.index, 1);
+      ReorderedItems.splice(destination.index, 0, reorderedItem);
+      
+      setGallery(ReorderedItems)
+  }
   return (
     <div>
-        <h1> Image gallery!!!</h1>
+        <Typography variant='h2'
+        sx={{
+          textAlign:'center',
+          padding:'20px 0px'
+        }}>
+          Photo gallery
+        </Typography>
+        <div className='first-gallerycon'>
+          <Typography variant='h4'>Welcome 
+              <Typography variant=' h4'>username</Typography>
+          </Typography>
+
+          <div>
+            <span>Date:12-09-2023</span>
+            <Button>logout</Button>
+          </div>
+        </div>
+        <div className='second-gallerycon'>
+          <h3>Drag and drop to reorder images</h3>
+            
+                  <DragDropContext onDragEnd={handleDrag}>
+                      <Droppable droppableId='ROOT' type='group'>
+                        {(provided)=>(
+                          <div {...provided.droppableProps} ref={provided.innerRef} className='grid-container'>
+                                {gallery.map((item,index)=>{
+                                  return(
+                                  <Draggable key={index} draggableId={index.toString()} index={index}>
+                                    {(provided)=>(
+                                        <div  
+                                        className='grid-item'
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}>
+                                            <img src={item.src} alt={`${item.name} image`} className='item-img'/>
+                                            <div className='grid-item-innercon'>
+                                              <Typography variant='h4'> {item.name}</Typography>
+                                              <Typography variant='body2'>
+                                                {item.text}
+                                              </Typography>
+                                            </div>
+                                        </div>
+                                    )}
+                                  </Draggable>
+                                  )
+                                })}
+                          </div>
+                           
+                        )}
+
+                      </Droppable>
+                  </DragDropContext>
+            
+              
+
+      
+
+          
+        </div>
     </div>
   )
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,64 @@
+import moonPhases from '../assests/moonPhases_image.jpg'
+import beachImg1 from '../assests/beach_image1.jpg'
+import oceanImg from '../assests/ocean_image1.jpg'
+import spaceImg from '../assests/space_image1.jpg'
+import treeImg1 from '../assests/tree_image1.jpg'
+import treeImg2 from '../assests/tree_image2.jpg'
+import treeImg3 from '../assests/tree_image3.jpg'
+import moutainImg1 from '../assests/mountain_image1.jpg'
+import moutainImg2 from '../assests/mountain_image2.jpg'
+import moutainImg3 from '../assests/mountain_image3.jpg'
+
+
+export const imgGallery=[
+    {
+        name: 'Tree',
+        src: treeImg1,
+        text:"Lorem ipsum dolor sit amet consectetur,adipisicing elit. Ratione, blanditiis! Et, qui. "
+    },
+    {
+        name:'Ocean',
+        src:oceanImg,
+        text:"Lorem ipsum dolor sit amet consectetur,adipisicing elit. Ratione, blanditiis! Et, qui. "
+    },
+    {
+        name: 'Mountain',
+        src:moutainImg1,
+        text:"Lorem ipsum dolor sit amet consectetur,adipisicing elit. Ratione, blanditiis! Et, qui. "
+    },
+    {
+        name: 'Tree',
+        src: treeImg3,
+        text:"Lorem ipsum dolor sit amet consectetur,adipisicing elit. Ratione, blanditiis! Et, qui. "
+    },
+    {
+        name: 'Moon phases',
+        src:moonPhases,
+        text:"Lorem ipsum dolor sit amet consectetur,adipisicing elit. Ratione, blanditiis! Et, qui. "
+    },
+    {
+        name: 'Beach',
+        src: beachImg1,
+        text:"Lorem ipsum dolor sit amet consectetur,adipisicing elit. Ratione, blanditiis! Et, qui. "
+    },
+    {
+        name: 'Tree',
+        src: treeImg2,
+        text:"Lorem ipsum dolor sit amet consectetur,adipisicing elit. Ratione, blanditiis! Et, qui. "
+    },
+    {
+        name:'Mountain',
+        src: moutainImg2,
+        text:"Lorem ipsum dolor sit amet consectetur,adipisicing elit. Ratione, blanditiis! Et, qui. "
+    },
+    {
+        name: 'Space',
+        src: spaceImg,
+        text:"Lorem ipsum dolor sit amet consectetur,adipisicing elit. Ratione, blanditiis! Et, qui. "
+    },
+    {
+        name: 'Mountain',
+        src: moutainImg3,
+        text:"Lorem ipsum dolor sit amet consectetur,adipisicing elit. Ratione, blanditiis! Et, qui. "
+    }
+]
